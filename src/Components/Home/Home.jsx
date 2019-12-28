20f7a6b521cfff22fb5f26ffaefd8b5d57eddab3
ramkashyap17 <ramkashyap17@gmail.com>
2019-12-29 05:00:37 +0530

    Loading animation done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,149 @@
 import React from 'react';
 import '../../Stylesheets/App.css';
+import ReactAnime from 'react-animejs'
+
+const {Anime, stagger} = ReactAnime
+
+let step = 0;
 
 export default class Home extends React.Component {
   constructor(props){
     super(props)
+    this.updateStep = this.updateStep.bind(this)
+  }
+
+  componentDidMount(){
+    setTimeout(function(){ step = 1; }, 5000);
+    // setTimeout(this.updateStep(1), 5000)
+    // setTimeout(this.updateStep(2), 8000)
+    // setTimeout(this.updateStep(3), 13000)
+  }
+
+  updateStep(s){
+    step = s;
   }
 
   render(){
     return (
       <div className="content">
         <div className="home">
-          <div className="home-header">
-            <h1 className="home-header__text">Please Wait...</h1>
+        {
+          step === 0 &&
+          <Anime
+          initial={[
+            {
+              targets: "#circle",
+              keyframes: [
+                {
+                  translateX: 50
+                },
+                {
+                  translateY: 50
+                },
+                {
+                  translateX: 0
+                },
+                {
+                  translateY: 0
+                }
+              ],
+              // easing:'spring',
+              duration: 3500,
+              loop: true
+            }
+          ]}
+          >
+            <div id="circle" className="home-subheader" style={{ marginBottom: 50, marginRight: 50, height: 20, width: 20, background: "#bc13fe", borderRadius: "50%" }}/>
+          </Anime>
+        }
+        {
+          step === 0 &&
+          <Anime
+          initial={[
+            {
+              targets: "#circle",
+              keyframes: [
+                {
+                  translateX: 50
+                },
+                {
+                  translateY: 50
+                },
+                {
+                  translateX: 0
+                },
+                {
+                  translateY: 0
+                }
+              ],
+              // easing:'spring',
+              duration: 3500,
+              loop: true
+            }
+          ]}
+          >
+            <div id="circle" className="home-subheader" style={{ marginBottom: 50, marginRight: 50, height: 30, width: 30, background: "#39ff14", borderRadius: "50%" }}/>
+          </Anime>
+        }
+        {
+          step === 0 &&
+          <Anime
+          initial={[
+            {
+              targets: "#circle3",
+              keyframes: [
+                {
+                  translateX: 50,
+                  color: "#fff",
+                  backgroundColor: ["rgba(55, 255, 20, 1)", "rgba(20, 255, 251, 1)"],
+                  loop: true,
+                  direction: 'alternate',
+                },
+                {
+                  translateY: 50,
+                  color: "#ff0",
+                  backgroundColor: ["rgba(20, 255, 251, 1)", "rgba(255, 255, 20, 1)"],
+                  loop: true,
+                  direction: 'alternate',
+                },
+                {
+                  translateX: 0,
+                  color: "#f00",
+                  backgroundColor: ["rgba(255, 255, 20, 1)", "rgba(255, 157, 20, 1)"],
+                  loop: true,
+                  direction: 'alternate',
+                },
+                {
+                  translateY: 0,
+                  color: "#000",
+                  backgroundColor: ["rgba(255, 157, 20, 1)", "rgba(255, 20, 20, 1)"],
+                  loop: true,
+                  direction: 'alternate',
+                },
+              ],
+              // easing:'spring',
+              duration: 3500,
+              loop: true
+            }
+          ]}
+          >
+            <div id="circle3" className="home-subheader" style={{ marginBottom: 50, marginRight: 50, height: 40, width: 40, background: "#7d12ff", borderRadius: "50%" }}/>
+          </Anime>
+        }
+        {
+          step === 1 &&
+          <div id="Box" className="home-header">
+            <h1 className="home-header__text">PLEASE W A I T. . .</h1>
           </div>
-          <h1 className="home-subheader">Connection to random server 1.12.123.235</h1>
+        }
+        {
+          step === 2 &&
+          <h1 className="home-subheader">ssh -i random_key.pub random_server@1.12.123.235</h1>
+        }
+        {
+          step === 3 &&
           <div className="terminal">
-            <div className="terminal-header">
+            <div className="sterminal-header">
               <div className="tab-title">
                 <div className="close__button">
                   <img src="/cross-out.png" className="tab-title__close"/>
@@ -29,6 +157,7 @@ export default class Home extends React.Component {
               </div>
             </div>
           </div>
+        }
         </div>
       </div>
     );
